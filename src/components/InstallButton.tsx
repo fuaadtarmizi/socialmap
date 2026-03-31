286126f54493287a00140522991c6d57c95b50a0
fuaadtarmizi <fuaad2120@gmail.com>
2026-03-31 16:09:11 +0800

    test20

--- src/components/InstallButton.tsx
+++ src/components/InstallButton.tsx
@@ -26,7 +26,7 @@ export const InstallButton = () => {
   };
 
   return (
-    <div className="hidden lg:block">
+    <div className="">
       <button
         onClick={handleInstall}
         className=" w-full flex items-center justify-center gap-2 py-3 rounded-xl bg-white/5 border border-white/10 text-white text-sm font-semibold hover:bg-white/10 transition-colors active:scale-95 transition-transform"
